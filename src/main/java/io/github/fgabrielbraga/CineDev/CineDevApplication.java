4b137129984edfe21eeba98eae56a78e7fc9dca5
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-03 22:38:15 -0300

    feat: project configuration

---
+++ src/main/java/io/github/fgabrielbraga/CineDev/CineDevApplication.java
@@ -0,0 +1,13 @@
+package io.github.fgabrielbraga.CineDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fgabrielbraga/CineDev/CineDevApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fgabrielbraga.CineDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
