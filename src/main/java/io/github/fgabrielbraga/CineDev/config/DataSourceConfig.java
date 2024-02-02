ee31fd7e7a894104610daa36232878f3b27ec4b4
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2024-02-02 10:51:17 -0300

    feat: environment variables configuration

--- src/main/java/io/github/fgabrielbraga/CineDev/config/DataSourceConfig.java
+++ src/main/java/io/github/fgabrielbraga/CineDev/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package io.github.fgabrielbraga.CineDev.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,19 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
+    @Value("${db-url}")
+    private String dbUrl;
+    @Value("${db-user}")
+    private String dbUser;
+    @Value("${db-password}")
+    private String dbPassword;
+
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://127.0.0.1:3306/cinedev_db");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("root3306");
+        dataSourceBuilder.url(dbUrl);
+        dataSourceBuilder.username(dbUser);
+        dataSourceBuilder.password(dbPassword);
         return dataSourceBuilder.build();
     }
 }
