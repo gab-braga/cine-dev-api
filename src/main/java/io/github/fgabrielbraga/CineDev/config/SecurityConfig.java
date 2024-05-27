3aecffdf4d5c2b63d091324c02cf3cf61587f3ab
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2024-05-27 19:53:44 -0300

    bug: remove ssl

--- src/main/java/io/github/fgabrielbraga/CineDev/config/SecurityConfig.java
+++ src/main/java/io/github/fgabrielbraga/CineDev/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -52,7 +51,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
